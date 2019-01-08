2aef8e36d20be18a64418188f3d2ce70c885f337
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-01-08 11:36:17 -0700

    adds 401 status to user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::UsersController < ActionController::API
 
   def create
     user = User.new(user_params)
-    render status: 201, json: {api_key: user.api_key} if user.save
+    if user.save
+      render status: 201, json: {api_key: user.api_key}
+    else
+      render status: 401
+    end
   end
 
   private
