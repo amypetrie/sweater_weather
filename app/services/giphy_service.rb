62eec1cf7686b27f4c4c9dacd9f241577fb0fdde
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-01-09 13:07:09 -0700

    finishes adding sad path tests

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -9,9 +9,8 @@ class GiphyService
   end
 
   def get_weather_gif
-    # input = parse_string_for_request(@filter[:weather])
-    # get_json("/v1/gifs/search?&q=#{input}")
-    get_random_gif
+    input = parse_string_for_request(@filter[:weather])
+    get_json("/v1/gifs/search?&q=#{input}")
   end
 
   def parse_string_for_request(string)
--- spec/services/giphy_service_spec.rb
+++ spec/services/giphy_service_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Giphy API Service" do
   it "returns a random gif" do
-    VCR.use_cassette("giphy_api_results") do
+    VCR.use_cassette("giphy_random_api_results") do
 
       filter = {weather: "weather"}
       service = GiphyService.new(filter)
@@ -16,5 +16,23 @@ describe "Giphy API Service" do
       expect(results[:data][:images][:fixed_height_still]).to have_key :url
     end
   end
+
+  it "returns a weather gif" do
+    VCR.use_cassette("giphy_queried_api_results") do
+
+      filter = {weather: "weather"}
+      service = GiphyService.new(filter)
+
+      results = service.get_weather_gif
+
+      expect(results).to be_a Hash
+      expect(results).to have_key :data
+      expect(results[:data].first).to have_key :images
+      expect(results[:data].first[:images]).to have_key :fixed_height_still
+      expect(results[:data].first[:images][:fixed_height_still]).to have_key :url
+    end
+  end
+
+
 
 end
