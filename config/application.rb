f0b35a6232c48a8a0acb01167c7428a5b65150e5
amypetrie <apetrie9849@gmail.com>
2019-01-23 13:41:06 -0700

    Change CORS origins

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,8 @@ module SweaterWeather
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200', 'https://amypetrie.github.io/weather_bug_frontend/']
+        # origins ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200', 'https://amypetrie.github.io/weather_bug_frontend/']
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
